336c81c2350064cc924644b0b2658a3ce6dd05b8
Carl Byström <cbystrom@ea.com>
2013-05-21 17:39:41 +0200

    Added missing class.

---
+++ src/main/java/com/cgbystrom/sockjs/transports/TransportMetrics.java
@@ -0,0 +1,41 @@
+package com.cgbystrom.sockjs.transports;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+
+/**
+* Created with IntelliJ IDEA.
+* User: cbystrom
+* Date: 2013-05-21
+* Time: 16:26
+* To change this template use File | Settings | File Templates.
+*/
+public class TransportMetrics {
+    public final Counter sessionsOpen;
+    public final Meter sessionsOpened;
+    public final Counter connectionsOpen;
+    public final Meter connectionsOpened;
+    public final Meter messagesReceived;
+    public final Histogram messagesReceivedSize;
+    public final Meter messagesSent;
+    public final Histogram messagesSentSize;
+    private final String transport;
+
+    public TransportMetrics(String transport, MetricRegistry metrics) {
+        this.transport = transport;
+        sessionsOpen = metrics.counter(getName("sessionsOpen"));
+        sessionsOpened = metrics.meter(getName("sessionsOpened"));
+        connectionsOpen = metrics.counter(getName("connectionsOpen"));
+        connectionsOpened = metrics.meter(getName("connectionsOpened"));
+        messagesReceived = metrics.meter(getName("messagesReceived"));
+        messagesReceivedSize = metrics.histogram(getName("messagesReceivedSize"));
+        messagesSent = metrics.meter(getName("messagesSent"));
+        messagesSentSize = metrics.histogram(getName("messagesSentSize"));
+    }
+
+    private String getName(String name) {
+        return MetricRegistry.name("com.cgbystrom.sockjs.transports", transport, name);
+    }
+}
